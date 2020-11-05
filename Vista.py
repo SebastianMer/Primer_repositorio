07bce189f844edffe966f4794b31be40ef75cc1c
Sebastian <sebastian.merchan01@correo.usa.edu.co>
2020-11-04 21:39:43 -0500

    Interfaz_sin_comandos

--- Vista.py
+++ Vista.py
@@ -7,7 +7,7 @@ def botones():
     b1.config(font=('Bahnschrift', 10))
     b1.pack()
     b1.place(x=290, y=200)
-    b2 = Button(frame, text="Paralelo", width="15", height='5',command=lambda: paralelo("paralelo"))
+    b2 = Button(frame, text="Paralelo", width="15", height='5', command=lambda: paralelo("paralelo"))
     b2.config(font=('Bahnschrift', 10))
     b2.pack()
     b2.place(x=290, y=400)
@@ -26,22 +26,52 @@ def serie(condicion):
         frame2.pack()
         frame2.config(bg='gray8')
 
-        # Titulo de la ventana
+        # Titulos
         titulo2 = Label(frame2, text='Circuito Serie')
         titulo2.config(fg='white', bg='gray8', font=('Roman', 20))
         titulo2.pack()
         titulo2.place(x=250, y=0)
 
+        # R1
+        ts1 = Label(frame2, text='R1(Ω)')
+        ts1.config(fg='white', bg='gray8', font=('Roman', 15))
+        ts1.pack()
+        ts1.place(x=350, y=50)
+        es1 = Entry(frame2)
+        es1.pack()
+        es1.place(x=410, y=53)
+
+        # R2
+        ts2 = Label(frame2, text='R2(Ω)')
+        ts2.config(fg='white', bg='gray8', font=('Roman', 15))
+        ts2.pack()
+        ts2.place(x=350, y=100)
+        es2 = Entry(frame2)
+        es2.pack()
+        es2.place(x=410, y=103)
+
         # Imagen
         cserie0 = PhotoImage(file='Serie_0.gif')
         lb = Label(frame2, imag=cserie0)
         lb.pack()
-        lb.place(x=180,y=100)
+        lb.place(x=0, y=50)
+
+        # Botones
+        bs1 = Button(frame2, text="+", width="5", height='5')
+        bs1.config(font=('Bahnschrift', 10))
+        bs1.pack()
+        bs1.place(x=0, y=350)
+        bs2 = Button(frame2, text="-", width="5", height='5')
+        bs2.config(font=('Bahnschrift', 10))
+        bs2.pack()
+        bs2.place(x=45, y=350)
+
+        # labels
 
         ventana2.mainloop()
 
-def paralelo(condicion):
 
+def paralelo(condicion):
     if condicion == "paralelo":
         # Ventana hija 2
         ventana3 = Toplevel()
@@ -64,9 +94,38 @@ def paralelo(condicion):
         cparalelo0 = PhotoImage(file='Paralelo_0.gif')
         lb = Label(frame3, imag=cparalelo0)
         lb.pack()
-        lb.place(x=180,y=100)
+        lb.place(x=0, y=50)
 
+        # Botones
+        bp1 = Button(frame3, text="+", width="5", height='5')
+        bp1.config(font=('Bahnschrift', 10))
+        bp1.pack()
+        bp1.place(x=0, y=350)
+        bp2 = Button(frame3, text="-", width="5", height='5')
+        bp2.config(font=('Bahnschrift', 10))
+        bp2.pack()
+        bp2.place(x=45, y=350)
+
+        # R1
+        tp1 = Label(frame3, text='R1(Ω)')
+        tp1.config(fg='white', bg='gray8', font=('Roman', 15))
+        tp1.pack()
+        tp1.place(x=350, y=50)
+        ep1 = Entry(frame3)
+        ep1.pack()
+        ep1.place(x=410, y=53)
+
+        # R2
+        tp2 = Label(frame3, text='R2(Ω)')
+        tp2.config(fg='white', bg='gray8', font=('Roman', 15))
+        tp2.pack()
+        tp2.place(x=350, y=100)
+        ep2 = Entry(frame3)
+        ep2.pack()
+        ep2.place(x=410, y=103)
         ventana3.mainloop()
+
+
 # Creación de ventana principal
 ventana = Tk()
 ventana.title("Simulador circuitos resistivos")
