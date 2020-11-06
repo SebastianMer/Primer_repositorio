02985af717e0e81d6198da3f552090020768124c
Sebastian <sebastian.merchan01@correo.usa.edu.co>
2020-11-06 17:19:25 -0500

    Interfaz_con_comandos

--- Simulador.py
+++ Simulador.py
@@ -7,6 +7,21 @@ Sebastián Merchán
 """
 
 
+def operacions(Res1, Res2, Res3, Res4, Res5, Res6, Res7):
+    print("1: " + str(Res1))
+    print("2: " + str(Res2))
+    print("3: " + str(Res3))
+    print("4: " + str(Res4))
+    print("5: " + str(Res5))
+    print("6: " + str(Res6))
+    print("7: " + str(Res7))
 
 
-
+def operacionp(Rep1, Rep2, Rep3, Rep4, Rep5, Rep6, Rep7):
+    print("1: " + str(Rep1))
+    print("2: " + str(Rep2))
+    print("3: " + str(Rep3))
+    print("4: " + str(Rep4))
+    print("5: " + str(Rep5))
+    print("6: " + str(Rep6))
+    print("7: " + str(Rep7))
--- Vista.py
+++ Vista.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from Simulador import operacions
+from Simulador import operacionp
 
 # Vaiables globales
 # Posiciones
@@ -8,9 +10,23 @@ poses = 153
 posep = 153
 conteo = 0
 conteop = 0
+
+# Variables circuitos serie
+lis = []
+Pis = []
+lser = []
+eser = []
+ops = []
 Rs = ['R3(Ω)', 'R4(Ω)', 'R5(Ω)', 'R6(Ω)', 'R7(Ω)']
-Rp = ['R3(Ω)', 'R4(Ω)', 'R5(Ω)', 'R6(Ω)', 'R7(Ω)']
 Is = ['Serie_1.gif', 'Serie_2.gif', 'Serie_3.gif', 'Serie_4.gif', 'Serie_5.gif']
+
+# Variables circuitos paralelo
+lip = []
+Pip = []
+lpar = []
+epar = []
+opp = []
+Rp = ['R3(Ω)', 'R4(Ω)', 'R5(Ω)', 'R6(Ω)', 'R7(Ω)']
 Ip = ['Paralelo_1.gif', 'Paralelo_2.gif', 'Paralelo_3.gif', 'Paralelo_4.gif', 'Paralelo_5.gif']
 
 
@@ -27,17 +43,20 @@ def botones():
 
 
 def serie(condicion):
-    global conteo, ventana2, lb
+    global conteo, ops
+
+    op = StringVar()
+    op2 = StringVar()
 
     if condicion == "serie":
         # Ventana hija
         ventana2 = Toplevel()
         ventana2.title("Circuito Serie")
-        ventana2.geometry("700x700+0+0")
+        ventana2.geometry("1200x1200+0+0")
         ventana2.config(bg='gray')
 
         # Creación del frame
-        frame2 = Frame(ventana2, width="700", height='700')
+        frame2 = Frame(ventana2, width="1200", height='1200')
         frame2.pack()
         frame2.config(bg='gray8')
 
@@ -51,60 +70,62 @@ def serie(condicion):
         ts1 = Label(frame2, text='R1(Ω)')
         ts1.config(fg='white', bg='gray8', font=('Roman', 15))
         ts1.pack()
-        ts1.place(x=450, y=50)
-        es1 = Entry(frame2)
+        ts1.place(x=670, y=50)
+        es1 = Entry(frame2, textvariable=op)
         es1.pack()
-        es1.place(x=550, y=53)
+        es1.place(x=750, y=53)
 
         # R2
         ts2 = Label(frame2, text='R2(Ω)')
         ts2.config(fg='white', bg='gray8', font=('Roman', 15))
         ts2.pack()
-        ts2.place(x=450, y=100)
-        es2 = Entry(frame2)
+        ts2.place(x=670, y=100)
+        es2 = Entry(frame2, textvariable=op2)
         es2.pack()
-        es2.place(x=550, y=103)
+        es2.place(x=750, y=103)
 
         # Imagen
         cserie1 = PhotoImage(file='Serie_0.gif')
-        lb = Label(frame2, imag=cserie1)
-        lb.pack()
-        lb.place(x=0, y=50)
+        lbs = Label(frame2, imag=cserie1)
+        lbs.pack()
+        lbs.place(x=0, y=50)
 
         # Botones
-        bs1 = Button(frame2, text="+", width="5", height='5', command=lambda: auxserie("+", frame2))
+        bs1 = Button(frame2, text="+", width="3", height='1', command=lambda: auxserie("+", frame2))
         bs1.config(font=('Bahnschrift', 10))
         bs1.pack()
-        bs1.place(x=0, y=380)
-        bs2 = Button(frame2, text="-", width="5", height='5')
+        bs1.place(x=0, y=410)
+        bs2 = Button(frame2, text="-", width="3", height='1')
         bs2.config(font=('Bahnschrift', 10))
         bs2.pack()
-        bs2.place(x=45, y=380)
+        bs2.place(x=30, y=410)
+        bs3 = Button(frame2, text="Resolver", width="7", height='1',
+                     command=lambda: operacions(op.get(), op2.get(), ops[0].get(), ops[1].get(), ops[2].get(),
+                                                ops[3].get(), ops[4].get()))
+        bs3.config(font=('Bahnschrift', 10))
+        bs3.pack()
+        bs3.place(x=60, y=410)
 
         ventana2.mainloop()
 
 
 def auxserie(cond, frame2):
-    global posts, Rs, poses, conteo, ventana2, Is, lb, ls, l1
+    global posts, Rs, poses, conteo, Is, lis, ops, lis, Pis
+
     if cond == '+' and conteo < 5:
+        ops.insert(conteo, StringVar())
+        lser.insert(conteo, Label(frame2, text=Rs[conteo]))
+        lser[conteo].config(fg='white', bg='gray8', font=('Roman', 15))
+        lser[conteo].pack()
+        lser[conteo].place(x=670, y=posts)
+        eser.insert(conteo, Entry(frame2, textvariable=ops[conteo]))
+        eser[conteo].pack()
+        eser[conteo].place(x=750, y=poses)
 
-        ts = Label(frame2, text=Rs[conteo])
-        ts.config(fg='white', bg='gray8', font=('Roman', 15))
-        ts.pack()
-        ts.place(x=450, y=posts)
-        es = Entry(frame2)
-        es.pack()
-        es.place(x=550, y=poses)
-
-        if conteo==0:
-            cserie0 = PhotoImage(file=Is[conteo])
-            cserie0.subsample(10,10)
-        else:
-            cserie0 = PhotoImage(file=Is[conteo])
-
-        l1 = Label(frame2, image=cserie0)
-        l1.pack()
-        l1.place(x=0, y=50)
+        Pis.insert(conteo, PhotoImage(file=Is[conteo]))
+        lis.insert(conteo, Label(frame2, image=Pis[conteo]))
+        lis[conteo].pack()
+        lis[conteo].place(x=0, y=50)
 
         posts += 50
         poses += 50
@@ -113,15 +134,18 @@ def auxserie(cond, frame2):
 
 
 def paralelo(condicion):
+    global opp, conteop
     if condicion == "paralelo":
+        ope = StringVar()
+        ope2 = StringVar()
         # Ventana hija 2
         ventana3 = Toplevel()
         ventana3.title("Circuito Paralelo")
-        ventana3.geometry("700x700+0+0")
+        ventana3.geometry("1200x1200+0+0")
         ventana3.config(bg='gray')
 
         # Creación del frame
-        frame3 = Frame(ventana3, width="700", height='700')
+        frame3 = Frame(ventana3, width="1200", height='1200')
         frame3.pack()
         frame3.config(bg='gray8')
 
@@ -137,51 +161,61 @@ def paralelo(condicion):
         lb.pack()
         lb.place(x=0, y=50)
 
-        # Botones
-        bp1 = Button(frame3, text="+", width="5", height='5', command=lambda: auxparalelo("+", frame3))
-        bp1.config(font=('Bahnschrift', 10))
-        bp1.pack()
-        bp1.place(x=0, y=350)
-        bp2 = Button(frame3, text="-", width="5", height='5')
-        bp2.config(font=('Bahnschrift', 10))
-        bp2.pack()
-        bp2.place(x=45, y=350)
-
         # R1
         tp1 = Label(frame3, text='R1(Ω)')
         tp1.config(fg='white', bg='gray8', font=('Roman', 15))
         tp1.pack()
-        tp1.place(x=390, y=50)
-        ep1 = Entry(frame3)
+        tp1.place(x=450, y=50)
+        ep1 = Entry(frame3, textvariable=ope)
         ep1.pack()
-        ep1.place(x=450, y=53)
+        ep1.place(x=505, y=53)
 
         # R2
         tp2 = Label(frame3, text='R2(Ω)')
         tp2.config(fg='white', bg='gray8', font=('Roman', 15))
         tp2.pack()
-        tp2.place(x=390, y=100)
-        ep2 = Entry(frame3)
+        tp2.place(x=450, y=100)
+        ep2 = Entry(frame3, textvariable=ope2)
         ep2.pack()
-        ep2.place(x=450, y=103)
+        ep2.place(x=505, y=103)
+
+        # Botones
+        bp1 = Button(frame3, text="+", width="3", height='1', command=lambda: auxparalelo("+", frame3))
+        bp1.config(font=('Bahnschrift', 10))
+        bp1.pack()
+        bp1.place(x=0, y=405)
+        bp2 = Button(frame3, text="-", width="3", height='1')
+        bp2.config(font=('Bahnschrift', 10))
+        bp2.pack()
+        bp2.place(x=30, y=405)
+        bp3 = Button(frame3, text='Resolver', width='7', height='1',
+                     command=lambda: operacionp(ope.get(), ope2.get(), opp[0].get(),
+                                                opp[1].get(), opp[2].get(),
+                                                opp[3].get(), opp[4].get()))
+        bp3.config(font=('Bahnschrift', 10))
+        bp3.pack()
+        bp3.place(x=60, y=405)
+
         ventana3.mainloop()
 
 
 def auxparalelo(condi, frame3):
-    global Rp, posep, postp, conteop, Ip
-    if condi == '+' and conteop < 5:
-        ts = Label(frame3, text=Rp[conteop])
-        ts.config(fg='white', bg='gray8', font=('Roman', 15))
-        ts.pack()
-        ts.place(x=390, y=postp)
-        es = Entry(frame3)
-        es.pack()
-        es.place(x=450, y=posep)
+    global Rp, posep, postp, conteop, Ip, lip, Pip, epar, opp
 
-        cparalelo = PhotoImage(file=Ip[conteop])
-        l2 = Label(frame3, image=cparalelo)
-        l2.pack()
-        l2.place(x=0, y=50)
+    if condi == '+' and conteop < 5:
+        opp.insert(conteop, StringVar())
+        lpar.insert(conteop, Label(frame3, text=Rp[conteop]))
+        lpar[conteop].config(fg='white', bg='gray8', font=('Roman', 15))
+        lpar[conteop].pack()
+        lpar[conteop].place(x=450, y=postp)
+        epar.insert(conteop, Entry(frame3, textvariable=opp[conteop]))
+        epar[conteop].pack()
+        epar[conteop].place(x=505, y=posep)
+
+        Pip.insert(conteop, PhotoImage(file=Ip[conteop]))
+        lip.insert(conteop, Label(frame3, image=Pip[conteop]))
+        lip[conteop].pack()
+        lip[conteop].place(x=0, y=50)
         postp += 50
         posep += 50
         conteop += 1
@@ -205,7 +239,6 @@ titulo.config(fg='white', bg='gray8', font=('Roman', 20))
 titulo.pack()
 titulo.place(x=180, y=0)
 
-ls = [Label, Label, Label, Label]
 # Botones
 botones()
 ventana.mainloop()
