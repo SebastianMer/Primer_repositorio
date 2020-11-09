772c727da92e1566d7ef9c8d488f8c472cd2bafe
Sebastian <sebastian.merchan01@correo.usa.edu.co>
2020-11-09 18:14:47 -0500

    Corrientes_paralelo_Voltajes_serie_completos_sin_GUI

--- Simulador.py
+++ Simulador.py
@@ -7,40 +7,51 @@ Sebastián Merchán
 """
 
 # Varibles auxiliares solucion Serie
-impserie = 3
+impserie = 2
 auxi = 0
 add = []
+Vser = []
+operser = 0
+sumares = 0
 
 # Varibles auxiliares solucion Paralelo
 imparalelo = 2
 auxip = 0
 addp = []
 Ipar = []
-sumatotalpar = 0
 operapar = 0
 
 def operacions(voltaje, Res1, Res2, ops, conteo):
-    global impserie, auxi
+    global impserie, auxi, add, Vser, operser, sumares
 
-    print("Voltaje " + str(voltaje))
-    print("1: " + str(Res1))
-    print("2: " + str(Res2))
     auxi = conteo
     conteo = 0
 
+    sumares = float(Res1) + float(Res2)
     while conteo < auxi:
         add.insert(conteo, ops[conteo].get())
-        print(str(impserie) + ": " + str(add[conteo]))
+        sumares = sumares + float(add[conteo])
+        conteo += 1
+
+    operser = float(voltaje) * float(Res1) / sumares
+    Vser.insert(0, operser)
+    operser = float(voltaje) * float(Res2) / sumares
+    Vser.insert(1, operser)
+    conteo = 0
+
+    while conteo < auxi:
+        operser = float(voltaje) * float(add[conteo]) / sumares
+        Vser.insert(impserie, operser)
         conteo += 1
         impserie += 1
 
-    impserie = 3
+    print(str(Vser))
+
 def operacionp(voltaje, Rep1, Rep2, opp, conteop):
     global auxip, imparalelo, sumatotalpar, operapar
 
     auxip = conteop
     conteop = 0
-    sumatotalpar = int(Rep1) + int(Rep2)
     while conteop < auxip:
         addp.insert(conteop, opp[conteop].get())
         conteop += 1
